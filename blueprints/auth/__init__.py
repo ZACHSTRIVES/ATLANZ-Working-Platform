73ec46e1da535041f5addddddfc16a7676a23d35
Zach <wangziqi0325@gmail.com>
2021-05-15 02:11:57 +1200

    [feature] add staff

--- blueprints/auth/__init__.py
+++ blueprints/auth/__init__.py
@@ -4,6 +4,11 @@ auth_bp = Blueprint('auth', __name__)
 
 
 @auth_bp.route('/', methods=['GET', 'POST'])
-def login():
+def loginPage():
     if request.method == 'GET':
         return render_template('page-login.html')
+
+@auth_bp.route('/login',methods=['POST'])
+def login():
+
+    return redirect(url_for("dashboard.dashboard"))
