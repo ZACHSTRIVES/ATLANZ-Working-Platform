2bb9b5e40db2a370b658ee28aab0d6c8eb0708bd
Zach <wangziqi0325@gmail.com>
2021-06-01 01:22:54 +1200

    [bug] fixed inventory

--- blueprints/inventory/__init__.py
+++ blueprints/inventory/__init__.py
@@ -3,7 +3,8 @@ from blueprints.inventory.products import *
 
 inventory_bp = Blueprint('inventory', __name__)
 
-@inventory_bp.route('/inventory',methods=['GET','POST'])
+
+@inventory_bp.route('/inventory', methods=['GET', 'POST'])
 def inventory():
     # login_ = login_status()
     user = session.get("user")
@@ -11,13 +12,17 @@ def inventory():
         return redirect(url_for("auth.login"))
     if request.method == 'GET':
         products = list_all_products()
-        return render_template('inventory.html', products=products,user=user)
+        return render_template('inventory.html', products=products, user=user)
 
-@inventory_bp.route('/edit_products<barcode>',methods=['GET','POST','PUT'])
+
+@inventory_bp.route('/edit_products<barcode>', methods=['GET', 'POST', 'PUT'])
 def edit_products(barcode):
+    user = session.get("user")
+    if user is None:
+        return redirect(url_for("auth.login"))
     if request.method == 'GET':
-        products=get_products(barcode)
-        return render_template('editInventory.html', products=products)
+        products = get_products(barcode)
+        return render_template('editInventory.html', products=products, user=user)
     elif request.method == 'POST':
         product_name = request.form.get('product_name')
         Qty = request.form.get('Qty')
@@ -25,30 +30,38 @@ def edit_products(barcode):
         selling_price = request.form.get('selling_price')
         sql = "UPDATE products " \
               "SET product_name='%s',Qty=%s,cost_price=%s,selling_price=%s" \
-              " WHERE barcode=%s" % (product_name, Qty, cost_price, selling_price, barcode)
+              " WHERE barcode='%s'" % (product_name, Qty, cost_price, selling_price, barcode)
         change_product_db(sql)
-        products = list_all_products()
-        return render_template('inventory.html', products=products)
 
-@inventory_bp.route('/add_products',methods=['GET','POST','PUT'])
+        return redirect(url_for("inventory.inventory"))
+
+
+@inventory_bp.route('/add_products', methods=['GET', 'POST', 'PUT'])
 def add_products():
+    user = session.get("user")
+    if user is None:
+        return redirect(url_for("auth.login"))
     if request.method == 'GET':
-        return render_template('addInventory.html')
+        return render_template('addInventory.html', user=user)
     elif request.method == 'POST':
         barcode = request.form.get('barcode')
         product_name = request.form.get('product_name')
         Qty = request.form.get('Qty')
         cost_price = request.form.get('cost_price')
         selling_price = request.form.get('selling_price')
-        sql = "INSERT INTO products (barcode, product_name, Qty, cost_price, selling_price)" \
-              "VALUES ('%s', '%s', %s, %s, %s);" % (barcode, product_name, Qty, cost_price, selling_price)
+        sql = "INSERT INTO products (barcode, product_name, Qty, cost_price, selling_price) " \
+              " VALUES ('%s', '%s', %s, %s, %s);" % (barcode, product_name, Qty, cost_price, selling_price)
         change_product_db(sql)
-        products = list_all_products()
-        return render_template('inventory.html', products=products)
+
+
+        return redirect(url_for("inventory.inventory"))
+
 
 @inventory_bp.route('/delproducts<barcode>', methods=['GET', 'DELETE'])
 def del_product(barcode):
-    sql = 'delete from products where barcode=%s' % barcode
+    user = session.get("user")
+    if user is None:
+        return redirect(url_for("auth.login"))
+    sql = "delete from products where barcode='%s'" % barcode
     change_product_db(sql)
-    products = list_all_products()
-    return render_template('inventory.html', products=products)
+    return redirect(url_for("inventory.inventory"))
--- blueprints/inventory/products.py
+++ blueprints/inventory/products.py
@@ -1,5 +1,6 @@
 from config import db
 
+
 def list_all_products():
     try:
         cur = db.cursor()
@@ -12,16 +13,18 @@ def list_all_products():
     except Exception as e:
         print(e)
 
+
 def get_products(barcode):
     try:
         cur = db.cursor()
         db.ping(reconnect=True)
-        sql = 'SELECT * FROM products WHERE barcode=%s' % barcode
+        sql = "SELECT * FROM products WHERE barcode='%s'" % barcode
         cur.execute(sql)
         result = cur.fetchone()
         return result
     except Exception as e:
         print(e)
+
 
 def change_product_db(sql):
     try:
